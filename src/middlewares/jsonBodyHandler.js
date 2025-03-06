c790d4097c43a1474e9b1a439fcdc2e969772846
Thiago Soares <thiagocarv4@gmail.com>
2025-03-06 17:48:52 -0300

    criando middlewares e separando rotas

---
+++ src/middlewares/jsonBodyHandler.js
@@ -0,0 +1,20 @@
+export async function jsonBodyHandler(request, response) {
+  //Adiciona cada chunk
+  const buffers = []
+  //Coleta os chunks de dados da requisição
+  for await (const chunk of request) {
+    buffers.push(chunk)
+  }
+
+  try {
+    //Concatena os chunks e converte para STR. Em seguida, converte a STR para JSON.
+    request.body = JSON.parse(Buffer.concat(buffers).toString())
+  } catch (error) {
+    console.log(error)
+    request.body = null
+  } 
+
+  //Define o header de resposta como json
+  response.setHeader("Content-Type", "application/json")
+  
+}
